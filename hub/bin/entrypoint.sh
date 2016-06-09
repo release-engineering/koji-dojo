35c380c98e72c2ae9fe3b1a14e49ff2ec4de841f
Petr Sakař <psakar@redhat.com>
2016-06-09 22:20:27 +0200

    add koji-hub name into /etc/hosts in koji-hub

--- hub/bin/entrypoint.sh
+++ hub/bin/entrypoint.sh
@@ -5,5 +5,8 @@ setup.sh
 
 IP=$(find-ip.py)
 
+# add koji-hub to hosts if not present
+if grep -q -v "koji-hub" /etc/hosts; then echo ${IP} koji-hub >> /etc/hosts; fi
+
 echo "Starting HTTPd on ${IP}"
 httpd -D FOREGROUND
