4534338bdbeec959b26bbbcfd1ff4743f00f6e89
Petr Sakař <psakar@redhat.com>
2016-06-10 16:29:20 +0200

    python-coverage is required
    newer version than available in rpm have to be installed

--- hub/bin/build-koji.sh
+++ hub/bin/build-koji.sh
@@ -6,6 +6,13 @@ if [ ! -d "/opt/koji/.git" ]; then
     git clone https://pagure.io/koji.git /opt/koji
 fi
 
+# install the latest version of python-coverage module
+wget https://bootstrap.pypa.io/ez_setup.py
+python ez_setup.py
+wget https://pypi.python.org/packages/2d/10/6136c8e10644c16906edf4d9f7c782c0f2e7ed47ff2f41f067384e432088/coverage-4.1.tar.gz#md5=80e63edaf49f689d304898fafc1007a5
+easy_install coverage-4.1.tar.gz
+rm -f coverage-4.1.tar.gz
+
 cd /opt/koji
 # Remove previous build to avoid multilib errors.
 rm -rf noarch
